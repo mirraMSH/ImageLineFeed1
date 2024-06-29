8a5531f13bc75d8b1229bd23571ce60bc8c5b860
Maria <manya0705@yandex.ru>
2024-06-30 02:41:37 +0300

    13 sprint

--- ImageFeed/AuthConfiguration.swift
+++ ImageFeed/AuthConfiguration.swift
@@ -11,10 +11,10 @@ enum Constants {
     
     //     высока вероятность того, что у меня больше нет запросов на старые ключи, поэтому здесь прилагаю новые (старые ключи закомментированы)
     
-    //    static let accessKey = "SY4PRl8nSlOVvHCHcJ_JEkJvm9OYnlA_zjMlJfkXM1k"
-    //    static let secretKey = "-8zQN7XZfmlU5k4395ig6cZlS41z23a_i2wUbVMY1kE"
-    static let accessKey = "E0IHLO11qpfBLhXeqW7aPkZsXgyrGN1to1cKwP_W3hI"
-    static let secretKey = "90S7iNwQC9NzuKIuoBqtXcmEhm_qK_KTnBuga9Mcfrg"
+    static let accessKey = "SY4PRl8nSlOVvHCHcJ_JEkJvm9OYnlA_zjMlJfkXM1k"
+    static let secretKey = "-8zQN7XZfmlU5k4395ig6cZlS41z23a_i2wUbVMY1kE"
+//    static let accessKey = "E0IHLO11qpfBLhXeqW7aPkZsXgyrGN1to1cKwP_W3hI"
+//    static let secretKey = "90S7iNwQC9NzuKIuoBqtXcmEhm_qK_KTnBuga9Mcfrg"
     static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
     static let accessScope = "public+read_user+write_likes"
     static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
--- ImageFeedUITests/ImageFeedUITests.swift
+++ ImageFeedUITests/ImageFeedUITests.swift
@@ -36,8 +36,10 @@ final class ImageFeedUITests: XCTestCase {
         XCTAssertTrue(passwordTextField.waitForExistence(timeout: 5))
         
         passwordTextField.tap()
+        
         passwordTextField.typeText("QWE321qwe") //ввести пароль
         //        webView.swipeUp()
+        sleep(5)
         XCUIApplication().toolbars.buttons["Done"].tap()
         
         webView.buttons["Login"].tap()
@@ -47,48 +49,52 @@ final class ImageFeedUITests: XCTestCase {
         
         sleep(5)
         
-//        XCTAssertTrue(cell.waitForExistence(timeout: 5))
+        XCTAssertTrue(cell.waitForExistence(timeout: 5))
         
     }
     
     func testFeed() throws {
+       
         sleep(3)
         let tablesQuery = app.tables
-        
+        sleep(3)
         let cell = tablesQuery.children(matching: .cell).element(boundBy: 0)
+        
+        sleep(3)
         cell.swipeUp()
         
         sleep(3)
         
         let cellToLike = tablesQuery.children(matching: .cell).element(boundBy: 1)
-        
+        sleep(5)
         cellToLike.buttons["LikeButton"].tap()
         sleep(5)
         cellToLike.buttons["LikeButton"].tap()
         
-        sleep(2)
+        sleep(5)
         
         cellToLike.tap()
         
-        sleep(2)
+        sleep(5)
         
         let image = app.scrollViews.images.element(boundBy: 0)
-        // Zoom in
+        
+        sleep(5)
+        
         image.pinch(withScale: 3, velocity: 1) // zoom in
-        // Zoom out
         image.pinch(withScale: 0.5, velocity: -1)
         
         let navBackButtonWhiteButton = app.buttons["backButtonWhite"]
         navBackButtonWhiteButton.tap()
+        sleep (5)
     }
     
     func testProfile() throws {
-        sleep(3)
+        sleep(5)
         app.tabBars.buttons.element(boundBy: 1).tap()
         
-        XCTAssertTrue(app.staticTexts["NameLSurnameLabel"].exists) // ввести имя+фамилия
-        XCTAssertTrue(app.staticTexts["@username"].exists) // ввести @логин
-        
+        XCTAssertTrue(app.staticTexts["NameLSurnameLabel"].exists)
+        XCTAssertTrue(app.staticTexts["@username"].exists) 
         app.buttons["Exit"].tap()
         
         app.alerts["Пока, пока!"].scrollViews.otherElements.buttons["Да"].tap()
